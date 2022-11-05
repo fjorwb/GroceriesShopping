1f3b9b498acea8de04ba27f619788e59bdc17dba
FJORWB <88600252+fjorwb@users.noreply.github.com>
2022-11-04 19:42:31 -0500

    Setting up Sequelize, Models, Routes

--- models/users.js
+++ models/users.js
@@ -1,16 +1,17 @@
 'use strict'
 const { Model } = require('sequelize')
-
 module.exports = (sequelize, DataTypes) => {
-	class User extends Model {
+	class users extends Model {
 		/**
 		 * Helper method for defining associations.
 		 * This method is not a part of Sequelize lifecycle.
 		 * The `models/index` file will call this method automatically.
 		 */
-		static associate(models) {}
+		static associate(models) {
+			// define association here
+		}
 	}
-	User.init(
+	users.init(
 		{
 			id: {
 				type: DataTypes.INTEGER,
@@ -18,30 +19,23 @@ module.exports = (sequelize, DataTypes) => {
 				autoIncrement: true,
 				allowNull: false
 			},
+			username: {
+				type: DataTypes.STRING,
+				allowNull: false
+			},
 			email: {
 				type: DataTypes.STRING,
-				allowNull: false,
-				unique: true,
-				validate: { isEmail: { msg: 'Email is not valid' } }
+				allowNull: false
 			},
 			password: {
 				type: DataTypes.STRING,
-				allowNull: false,
-				validate: {
-					notEmpty: false,
-					len: { args: [8, 100], msg: 'Password must be between 8 and 20 characters' }
-				}
-			},
-			username: {
-				type: DataTypes.STRING,
-				allowNull: false,
-				validate: { isAlpha: { msg: 'username name must be letters only' } }
+				allowNull: false
 			}
 		},
 		{
 			sequelize,
-			modelName: 'User'
+			modelName: 'users'
 		}
 	)
-	return User
+	return users
 }
--- server.js
+++ server.js
@@ -1,22 +1,44 @@
+require('dotenv').config()
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const app = express()
 
-const usersRouter = require('./routes/users')
+// const Sequelize = require('sequelize')
+// sequelize = new Sequelize(process.env.DATABASE_URL, {
+// 	dialectOptions: {
+// 		ssl: {
+// 			require: true,
+// 			rejectUnauthorized: false
+// 		}
+// 	}
+// })
+
+const { sequelize } = require('./models/index')
 
 // Middleware
 app.use(bodyParser.json())
 app.use(cors())
 
 // Routes
-app.use('/users', usersRouter)
 
 app.get('/', (req, res) => {
-	res.send('Hello World!')
+	res.send('Hello World!!!!!!!!!!!!!!!!!!')
 })
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => console.log(`Server started on port http://localhost:${PORT}`))
+app.listen(PORT, () => {
+	console.log(`Server running on port http://localhost:${PORT}`)
+})
+
+sequelize
+	.authenticate()
+	.then(() => {
+		console.log('Connection has been established successfully.')
+	})
+	.catch(err => {
+		console.error('Unable to connect to the database:', err)
+	})

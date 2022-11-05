6f8ffd9fd8ffdffbdf8404b88a68d5db8e9010de
FJORWB <88600252+fjorwb@users.noreply.github.com>
2022-11-05 13:14:56 -0500

    Testing Database deployment on Heroku

--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,6 @@ const usersRouter = express.Router()
 const userController = require('../controllers/usersControllers')
 
 usersRouter.get('/', userController.getAllUsers)
+usersRouter.get('/:id', userController.getUserById)
 
 module.exports = usersRouter

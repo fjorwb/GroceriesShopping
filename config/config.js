087970fc0d170598080f1dd9cb1643d6ae874234
FJORWB <88600252+fjorwb@users.noreply.github.com>
2022-11-16 10:20:28 -0500

    Coding users's CRUD

--- config/config.js
+++ config/config.js
@@ -22,10 +22,6 @@ module.exports = {
 	production: {
 		use_env_variable: 'DATABASE_URL',
 		dialect: 'postgres',
-		define: {
-			underscored: true
-		},
-
 		dialectOptions: {
 			ssl: {
 				require: true,

f48f6b4f094a50c475fd410e8141a6042df23c11
FJORWB <88600252+fjorwb@users.noreply.github.com>
2022-12-13 08:40:06 -0500

    setup user/recipe association

--- database/seeders/seed-recipes.js
+++ database/seeders/seed-recipes.js
@@ -12,6 +12,7 @@ module.exports = {
 					image: 'https://www.themealdb.com/images/media/meals/58oia61564916529.jpg',
 					servings: 4,
 					instructions: 'Instructions for Recipe 1',
+					user_id: 1,
 					created_at: new Date(),
 					updated_at: new Date()
 				},
@@ -21,6 +22,7 @@ module.exports = {
 					image: 'https://www.themealdb.com/images/media/meals/58oia61564916529.jpg',
 					servings: 4,
 					instructions: 'Instructions for Recipe 2',
+					user_id: 2,
 
 					created_at: new Date(),
 					updated_at: new Date()

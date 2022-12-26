33a191ac3b1ccfbb1502a8802e85aef52c887d52
FJORWB <88600252+fjorwb@users.noreply.github.com>
2022-12-26 14:10:05 -0500

    add factor field to menu model

--- models/Market.js
+++ models/Market.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
 		 * The `models/index` file will call this method automatically.
 		 */
 		static associate(models) {
-			Market.hasMany(models.User)
+			// Market.hasMany(models.User)
 		}
 	}
 	Market.init(
--- models/User.js
+++ models/User.js
@@ -9,9 +9,8 @@ module.exports = (sequelize, DataTypes) => {
 		 * The `models/index` file will call this method automatically.
 		 */
 		static associate(models) {
-			User.hasMany(models.Recipe),
-				// define association here
-				User.hasMany(models.Market)
+			User.hasMany(models.Recipe)
+			// , User.hasMany(models.Market)
 		}
 	}
 	User.init(

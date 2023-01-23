63dd2a48b8982dc723e549b558672672598c1e09
FJORWB <88600252+fjorwb@users.noreply.github.com>
2023-01-23 12:18:38 -0500

    review & change Menu controller

--- controllers/menusController.js
+++ controllers/menusController.js
@@ -1,12 +1,16 @@
-const { Menu } = require('../models')
+const {Menu} = require('../models')
 
 module.exports = {
 	async getAllMenus(req, res) {
-		const menus = await Menu.findAll()
-			.then(menus => {
+		const menus = await Menu.findAll({
+			where: {
+				user_id: req.user.id
+			}
+		})
+			.then((menus) => {
 				res.status(200).json(menus)
 			})
-			.catch(err => {
+			.catch((err) => {
 				res.status(500).json(err)
 			})
 	},
@@ -19,7 +23,7 @@ module.exports = {
 		})
 
 		if (!menu) {
-			return res.status(404).send({ message: 'Menu not found' })
+			return res.status(404).send({message: 'Menu not found'})
 		} else {
 			return res.status(200).send(menu)
 		}
@@ -27,10 +31,10 @@ module.exports = {
 
 	async createMenu(req, res) {
 		const menu = await Menu.create(req.body)
-			.then(menu => {
-				res.status(201).send({ message: 'meal created successfully', menu })
+			.then((menu) => {
+				res.status(201).send({message: 'meal created successfully', menu})
 			})
-			.catch(err => {
+			.catch((err) => {
 				res.status(500).json(err)
 			})
 	},
@@ -38,47 +42,47 @@ module.exports = {
 	async updateMenu(req, res) {
 		// console.log(req.params.id)
 
-		const checkMenu = await Menu.findOne({ where: { id: req.params.id } })
-			.then(menu => {
+		const checkMenu = await Menu.findOne({where: {id: req.params.id}})
+			.then((menu) => {
 				return menu
 			})
-			.catch(err => {
+			.catch((err) => {
 				res.status(500).json(err)
 			})
 
 		// console.log(checkMenu)
 
 		if (!checkMenu) {
-			return res.status(400).json({ message: 'meal not found' })
+			return res.status(400).json({message: 'meal not found'})
 		}
 
-		const menu = await Menu.update(req.body, { where: { id: req.params.id } })
-			.then(menu => {
-				res.status(200).send({ message: 'meal updated successfully' })
+		const menu = await Menu.update(req.body, {where: {id: req.params.id}})
+			.then((menu) => {
+				res.status(200).send({message: 'meal updated successfully'})
 			})
-			.catch(err => {
+			.catch((err) => {
 				res.status(500).json(err)
 			})
 	},
 
 	async deleteMenu(req, res) {
 		const checkMenu = await Menu.findByPk(req.params.id)
-			.then(menu => {
+			.then((menu) => {
 				return menu
 			})
-			.catch(err => {
+			.catch((err) => {
 				res.status(500).json(err)
 			})
 
 		if (!checkMenu) {
-			return res.status(400).json({ message: 'meal does not exist' })
+			return res.status(400).json({message: 'meal does not exist'})
 		}
 
-		const menu = await Menu.destroy({ where: { id: req.params.id } })
-			.then(menu => {
-				res.status(200).send({ message: 'meal deleted successfully', menu })
+		const menu = await Menu.destroy({where: {id: req.params.id}})
+			.then((menu) => {
+				res.status(200).send({message: 'meal deleted successfully', menu})
 			})
-			.catch(err => {
+			.catch((err) => {
 				res.status(500).json(err)
 			})
 	}

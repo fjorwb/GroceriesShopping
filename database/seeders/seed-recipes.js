4da97cf12a40f3da7b7f5bff2bade8c78d99340c
FJORWB <88600252+fjorwb@users.noreply.github.com>
2023-01-27 19:57:55 -0500

    r&c recipes seed

--- database/seeders/seed-recipes.js
+++ database/seeders/seed-recipes.js
@@ -22,7 +22,7 @@ module.exports = {
 					image: 'https://www.themealdb.com/images/media/meals/58oia61564916529.jpg',
 					servings: 4,
 					instructions: 'Instructions for Recipe 2',
-					user_id: 2,
+					user_id: 1,
 					created_at: new Date(),
 					updated_at: new Date()
 				}
